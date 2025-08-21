21f0dbea9b3458fa31fd87779956da187351bfcf
Yoepee <kdy2975@naver.com>
2025-08-21 11:46:05 +0900

    init

---
+++ src/main/java/com/back/Aop250821Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aop250821Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aop250821Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/Aop250821ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aop250821ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
