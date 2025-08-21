f718ed6632bbb185d45fea1287b58cabd476ea12
Yoepee <kdy2975@naver.com>
2025-08-21 14:39:39 +0900

    Bean으로 runner 사용

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,5 +1,7 @@
 package com.back;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,5 +15,15 @@ public class AppConfig {
     @Bean
     int version() {
         return 3;
+    }
+
+    @Bean
+    public ApplicationRunner myApplicationRunner() {
+        return new ApplicationRunner() {
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                System.out.println("ApplicationRunner is running");
+            }
+        };
     }
 }
