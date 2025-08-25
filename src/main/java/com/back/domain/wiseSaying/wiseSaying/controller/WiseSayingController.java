bfd3aa0a8dedf012acc9e2e01f192a7c7d097ad2
Yoepee <kdy2975@naver.com>
2025-08-25 09:48:18 +0900

    명언 삭제

--- src/main/java/com/back/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/back/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
@@ -3,18 +3,22 @@ package com.back.domain.wiseSaying.wiseSaying.controller;
 import com.back.domain.wiseSaying.wiseSaying.entity.WiseSaying;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+@RequestMapping("/wiseSayings")
 @Controller
 public class WiseSayingController {
     private int lastId = 0;
     private final List<WiseSaying> wiseSayings = new ArrayList<>();
 
-    @GetMapping("/wiseSayings/write")
+    @GetMapping("/write")
     @ResponseBody
     public String write(String content, String author) {
         if (content == null || content.isBlank()) {
@@ -33,7 +37,7 @@ public class WiseSayingController {
         return "%d번 명언이 생성되었습니다.".formatted(id);
     }
 
-    @GetMapping("wiseSayings")
+    @GetMapping("")
     @ResponseBody
     public String list() {
         return "<ul>"
@@ -44,5 +48,22 @@ public class WiseSayingController {
                 )
                 .collect(Collectors.joining(""))
                 + "</ul>";
+    }
+    
+    @GetMapping("/delete/{id}")
+    @ResponseBody
+    public String delete(@PathVariable int id) {
+        WiseSaying wiseSaying = findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("%d번 명언이 없습니다.".formatted(id)));
+
+        wiseSayings.remove(wiseSaying);
+        
+        return "%d번 명언이 삭제되었습니다.".formatted(id);
+    }
+    
+    private Optional<WiseSaying> findById (int id) {
+        return wiseSayings.stream()
+                .filter(wiseSaying -> wiseSaying.getId() == id)
+                .findFirst();
     }
 }
